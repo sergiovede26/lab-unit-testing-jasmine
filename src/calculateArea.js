c272a8696d35b54e17880edf3375f767a5f3c767
Sergio Diaz <sadiazcaballero@gmail.com>
2024-04-13 09:37:50 +0200

    Edited 13.04.2024

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,10 @@
-function calculateArea() {
+function calculateArea(height,width) {
+    
+    if (typeof height !== 'number' || typeof width !== 'number') {
+        return undefined;
+    }  
 
+    return height * width;
 }
+
+divide(10, 5);
