43f676d0f4ff419cef3fd1f4338f6db2d3c1b932
Sergio Diaz <sadiazcaballero@gmail.com>
2024-04-13 09:38:34 +0200

    Edited 13.04.2024

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,9 @@
-function divide () {
-       
+function divide(dividend, divisor) {
+    if (typeof dividend !== 'number' || typeof divisor !== 'number') {
+        return undefined;
+    }  
+
+    return dividend / divisor;
 }
+
+divide(10, 5);
